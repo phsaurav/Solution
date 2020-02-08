619da720dbd815cafc261da28bf43fe285a49ffe
PH Saurav <phsaurav29@gmail.com>
2020-02-08 21:30:32 +0600

    Rabbi Push

--- acmX/HackerRank - Data Structures - Linked Lists/Print the Elements of a Linked List/sol.cpp
+++ acmX/HackerRank - Data Structures - Linked Lists/Print the Elements of a Linked List/sol.cpp
@@ -12,6 +12,41 @@ class Node
 
 Node* head = NULL;
 
+void addHead(int nData)
+{
+    Node* newNode = new Node();
+    
+}
+
+void del(int pos)
+{
+    Node *last = head;
+    Node *last2;
+    for(int i = 1; i < pos; i++)
+    {
+        last = last->next;
+    }
+    
+    last2 = last->next;
+
+    last->next = last2->next;
+
+}
+
+void insert(int data, int pos)
+{
+    Node *newNode = new Node();
+    Node *last = head;
+    newNode->data = data;
+    for(int i = 1; i < pos; i++)
+    {
+        last = last->next;
+    }
+
+    newNode->next = last->next;
+    last->next = newNode;
+}
+
 void add(int nData)
 {
     Node* newNode = new Node();
@@ -43,16 +78,11 @@ void print()
 
 int main()
 {
-    int n,temp;
-
-    cin >> n;
-
-    for(int i = 0; i < n; i++)
-    {
-        cin >> temp;
-        add(temp);
-    }
-
+    add(5);
+    add(6);
+    add(7);
+    insert(8,1);
+    del(2);
     print();
     return 0;
 }
